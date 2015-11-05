7cf9bcb071badcd68e5c46a048645af35fb45f5e
Huizi Mao <huizi@stanford.edu>
2015-11-04 22:55:31 -0800

    bug fixed

--- script/dump_spm.py
+++ script/dump_spm.py
@@ -16,7 +16,7 @@ def dump2file(mat, filename):
     m,n = mat.shape
     np.array([m,n,nnz], dtype = 'int32').tofile(f)
     csr_m.indptr.tofile(f)
-    mat.data.tofile(f)
+    csr_m.data.tofile(f)
     f.close()
 
 
@@ -26,23 +26,23 @@ layers = {'lenet5':['ip1', 'ip2'],
           'vgg': ['fc6', 'fc7', 'fc8']}
 
 nets = layers.keys()
-for net in nets:
-    if net == 'lenet5':                                             
+for net_name in nets:
+    if net_name == 'lenet5':                                             
         prototxt = '3_prototxt_solver/lenet5/train_val.prototxt'       
         caffemodel = '4_model_checkpoint/lenet5/lenet5.caffemodel'     
-    elif net == 'alexnet':                                          
+    elif net_name == 'alexnet':                                          
         prototxt = '3_prototxt_solver/L2/train_val.prototxt'           
         caffemodel = '4_model_checkpoint/alexnet/alexnet9x.caffemodel' 
-    elif net == 'vgg':
+    elif net_name == 'vgg':
         prototxt = '3_prototxt_solver/vgg16/train_val.prototxt'     
         caffemodel = '4_model_checkpoint/vgg16/vgg16_13x.caffemodel'
-    elif net == 'lenet_300':
+    elif net_name == 'lenet_300':
         prototxt = '3_prototxt_solver/lenet_300_100/train_val.prototxt'
         caffemodel = '4_model_checkpoint/lenet_300_100/lenet300_100_9x.caffemodel'
     net = caffe.Net(prototxt, caffemodel, caffe.TEST)
     net.forward()
-    for layer in layers[net]:
-        data_dir = 'data/%s_%s'%(net, layer)
+    for layer in layers[net_name]:
+        data_dir = 'data/%s_%s'%(net_name, layer)
         os.system('mkdir ' + data_dir)
         mat = net.params[layer][0].data
         dump2file(mat, data_dir + '/matrix.dat')
