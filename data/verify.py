411fa7e05b99ef7c59fd003f685507c838392e41
Huizi Mao <huizi@stanford.edu>
2015-11-14 20:37:32 -0800

    fixed verify.py problem

--- data/verify.py
+++ data/verify.py
@@ -2,8 +2,8 @@ import numpy as np
 out = np.fromfile(open('output.dat'), dtype=np.float32)
 out2 = np.fromfile(open('groundtruth.dat'), dtype=np.float32)
 act_len = len(out2)
-out = abs(out)
-out2 = abs(out2)
+out[out<0] = 0
+out2[out2<0] = 0
 if act_len > 200:
     print out[:50]-out2[:50]
     print out[act_len-50:act_len] - out2[act_len-50:]
